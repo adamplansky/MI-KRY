c1be8b3b3db15c704df4a51ae23957ac058f3bc9
Adam Plansky <adamplansky@gmail.com>
2015-10-20 20:36:28 +0200

    random generator glibc

--- ukol01_random_generator_glibc/main.c
+++ ukol01_random_generator_glibc/main.c
@@ -2,16 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#define STATELEN 200
+#define STATELEN 31
 int main(void)
 {
   int seed_number = 5;
+  int randNumber = 0;
+  int i = 0;
   char randomStateBuffer[STATELEN];
   struct random_data randData;
   memset(randomStateBuffer, 0, sizeof(randomStateBuffer));
   memset(&randData, 0, sizeof(struct random_data));
+
   initstate_r(seed_number, randomStateBuffer,sizeof(randomStateBuffer), &randData);
-  printf("%d\n", seed_number);
+  setstate_r(randomStateBuffer, &randData);
+  srandom_r(seed_number, &randData);
   
+  for(i = 0; i < 20; i++ )  
+  {
+  	random_r(&randData, &randNumber);
+  	printf("%d\n", randNumber);
+  }
+
   return 0;
 }
