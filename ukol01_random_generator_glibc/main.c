af77a116f03b3e29cd0d4cddefd46c9724033cef
Adam Plansky <adamplansky@gmail.com>
2015-10-20 18:33:13 +0200

    bug mac

---
+++ ukol01_random_generator_glibc/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define STATELEN 200
+int main(void)
+{
+  int seed_number = 5;
+  char randomStateBuffer[STATELEN];
+  struct random_data randData;
+  memset(randomStateBuffer, 0, sizeof(randomStateBuffer));
+  memset(&randData, 0, sizeof(struct random_data));
+  initstate_r(seed_number, randomStateBuffer,sizeof(randomStateBuffer), &randData);
+  
+  
+  return 0;
+}
